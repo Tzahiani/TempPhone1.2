ca47327fe87504d85be7ad15a816c7ab1d23ecf9
Tzahi Anidgar <tzahianidgar@Tzahis-MacBook-Pro.local>
2014-11-17 18:12:03 +0200

    Initial Commit

---
+++ TempPhone1.2/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  TempPhone1.2
+//
+//  Created by Tzahi Anidgar on 17/11/14.
+//  Copyright (c) 2014 GreatApp. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
